f91d7c2e1e15e78312598cf9e7771481a208092f
Gregwar <g.passault@gmail.com>
2012-06-24 01:53:14 +0200

    [HTTP] QueryString ne contient pas ?

--- pages/02_http.php
+++ pages/02_http.php
@@ -252,7 +252,7 @@ tableau <code>$_GET</code> :
 <?php echo $slidey->highlight('files/02/get.php'); ?>
 
 <p>
-    <code>?x=42&amp;y=1337</code> est ce que l'on apelle une <em>Query String</em></code>
+    <code>x=42&amp;y=1337</code> est ce que l'on apelle une <em>Query String</em></code>
 </p>
 
 </div>
