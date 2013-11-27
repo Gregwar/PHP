707c4b1170c1beb29fcff5bbc160e713138e618a
Gregwar <g.passault@gmail.com>
2013-11-27 12:06:35 +0100

    Removing useless cache

--- files/sources/td5/composer/index.php
+++ files/sources/td5/composer/index.php
@@ -3,9 +3,7 @@ include('vendor/autoload.php');
 
 $loader = new Twig_Loader_Filesystem('templates/');
 
-$twig = new Twig_Environment($loader, array(
-        'cache' => 'cache',
-    ));
+$twig = new Twig_Environment($loader);
 
 echo $twig->render('index.html.twig', array(
     'name' => 'Fabien'
