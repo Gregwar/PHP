09a535d1909b81b070f8c01fdb9abb9ac5c3987e
Gregwar <g.passault@gmail.com>
2015-01-01 21:46:18 +0100

    Register form

--- files/sources/td5/register.php
+++ files/sources/td5/register.php
@@ -2,4 +2,25 @@
 
 <h2>Inscription</h2>
 
+<form method="post" class="form-horizontal">
+    <div class="form-group">
+        <label class="col-sm-2" for="login">Identifiant</label>
+        <div class="col-sm-10">
+            <input class="form-control" type="text" id="login" name="login" />
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="col-sm-2" for="login">Mot de passe</label>
+        <div class="col-sm-10">
+            <input class="form-control" type="text" id="password" name="password" />
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-2">&nbsp;</div>
+        <div class="col-sm-10">
+            <input type="submit" class="btn btn-success" value="Enregistrer" />
+        </div>
+    </div>
+</form>
+
 <?php include('footer.php'); ?>
