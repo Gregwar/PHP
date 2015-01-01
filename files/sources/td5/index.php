d8fa7bb9ee56fc38418a65718b18773d72aeb50b
Gregwar <g.passault@gmail.com>
2015-01-01 21:47:01 +0100

    Moving to latin1

--- files/sources/td5/index.php
+++ files/sources/td5/index.php
@@ -2,14 +2,14 @@
 
 <h2>Bienvenue</h2>
 <p>
-    Bienvenue sur <em>Sondages Mania</em>, ici, vous pourrez crÃ©er et participer
-    Ã  des sondages!
+    Bienvenue sur <em>Sondages Mania</em>, ici, vous pourrez créer et participer
+    à des sondages!
 </p>
 
-<h2>Comment Ã§a marche?</h2>
+<h2>Comment ça marche?</h2>
 <p>
-    PremiÃ¨rement, <a href="register.php">inscrivez-vous</a>, vous devez avoir un 
-    compte pour participer. <a href="create.php">CrÃ©ez ensuite</a> votre sondage en ligne.
+    Premièrement, <a href="register.php">inscrivez-vous</a>, vous devez avoir un 
+    compte pour participer. <a href="create.php">Créez ensuite</a> votre sondage en ligne.
     Vous obtiendrez un lien, en allant dans la rubrique <a href="my-polls.php">mes sondages</a>
     que vous pourrez partager pour que les autres puissent participer.
 </p>
