0e410187e9c112116111d44bff525f38025f3ef5
Gregwar <g.passault@gmail.com>
2015-01-04 14:58:49 +0100

    Ajout erreur explicite

--- files/sources/td5/bdd/pdo.php
+++ files/sources/td5/bdd/pdo.php
@@ -1,8 +1,14 @@
 <?php
 
-$pdo = new \PDO(
-    'mysql:dbname=sondage;host=localhost',
-    'sondage', 'sondage'
-);
+try {
+    $pdo = new \PDO(
+        'mysql:dbname=sondage;host=localhost',
+        'sondage', 'sondage'
+    );
+} catch (\PDOException $e) {
+    header('Content-type: text/plain');
+    echo "Impossible de se connecter a la base de donnees\n";
+    die($e->getMessage());
+}
 
 $pdo->exec('SET CHARSET UTF8');
