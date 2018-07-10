16e58f692373d13501f8c9e9f661a1558ca5070d
Gregwar <g.passault@gmail.com>
2018-07-10 18:01:30 +0200

    Slim3 WIP

--- files/sources/td4/index.php
+++ files/sources/td4/index.php
@@ -3,56 +3,77 @@
 $loader = include('vendor/autoload.php');
 $loader->add('', 'src');
 
-use Symfony\Component\HttpFoundation\Request;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
-$app = new Silex\Application;
-$app->register(new Silex\Provider\AssetServiceProvider());
-$app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__.'/views',
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'hostname' => 'localhost',
+            'user' => 'cinema',
+            'password' => 'cinema',
+            'database' => 'cinema',
+        ]
+    ]
 ]);
 
-// Fait remonter les erreurs
-$app['debug'] = true;
+// Enregistrement de Twig
+$container = $app->getContainer();
 
-$app['model'] = new Cinema\Model(
-    'localhost',  // Hôte
-    'cinema',    // Base de données
-    'cinema',    // Utilisateur
-    'cinema'     // Mot de passe
-);
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__.'/views', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
+    return $view;
+};
+
+// Enregistrement de la base de données
+$container['model'] = function($app) {
+    $db = $app['settings']['db'];
+    return new Cinema\Model($db['hostname'], $db['database'], $db['user'], $db['password']);
+};
 
 // Page d'accueil
-$app->match('/', function() use ($app) {
-    return $app['twig']->render('home.html.twig');
-})->bind('home');
+$app->get('/', function(Request $request, Response $response, array $args) {
+    return $this->view->render($response, 'home.html.twig');
+})->setName('home');
 
 // Liste des films
-$app->match('/films', function() use ($app) {
-    return $app['twig']->render('films.html.twig', [
-        'films' => $app['model']->getFilms()
+$app->get('/films', function(Request $request, Response $response, array $args) {
+    return $this->view->render($response, 'films.html.twig', [
+        'films' => $this->model->getFilms()
     ]);
-})->bind('films');
+})->setName('films');
 
 // Fiche film
-$app->match('/fiche_film/{id}', function($id, Request $request) use ($app) {
-    if ($request->getMethod() == 'POST') {
-        $post = $request->request;
-        if ($post->has('nom') && $post->has('note') && $post->has('critique')) {
-            // XXX: A faire
-        }
-    }
+$app->get('/fiche_film/{id}', function(Request $request, Response $response, array $args) use ($app) {
+    $id = (int)$args['id'];
 
-    return $app['twig']->render('film.html.twig', [
-        'film' => $app['model']->getFilm($id),
-        'casting' => $app['model']->getCasting($id),
+    return $this->view->render($response, 'film.html.twig', [
+        'film' => $this->model->getFilm($id),
+        'casting' => $this->model->getCasting($id),
     ]);
-})->bind('film');
+})->setName('film');
+
+$app->post('/fiche_film/{id}', function(Request $request, Response $response,array $args) {
+    $post = $request->getParsedBody();
+    
+    throw new \Exception("A faire dans le TD!");
+
+    return $response->withRedirect($this->router->pathFor('film', $args));
+})->setName('film_critique');
 
 // Genres
-$app->match('/genres', function() use ($app) {
-    return $app['twig']->render('genres.html.twig', [
-        'genres' => $app['model']->getGenres()
+$app->get('/genres', function($request, $response, $args) {
+    return $this->view->render($response, 'genres.html.twig', [
+        'genres' => $this->model->getGenres()
     ]);
-})->bind('genres');
+})->setName('genres');
 
 $app->run();
